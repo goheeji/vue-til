4327115d4c204e8832ea351e52525a0708c32e59
goheeji <gmlwl0489@daum.net>
2022-01-26 11:34:40 +0900

    로그인, 회원가입 페이지 개발

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,60 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="text" v-model="password" />
+    </div>
+    <button :disabled="!isUsernameValid || !password" type="submit">
+      로그인
+    </button>
+    <p>{{ logMessage }}</p>
+  </form>
+</template>
+
+<script>
+import { loginUser } from '@/api/index';
+import { validateEmail } from '@/utils/validation';
+export default {
+  data() {
+    return {
+      // form values
+      username: '',
+      password: '',
+      // log
+      logMessage: '',
+    };
+  },
+  computed: {
+    isUsernameValid() {
+      return validateEmail(this.username);
+    },
+  },
+  methods: {
+    async submitForm() {
+      try {
+        const userData = {
+          username: this.username,
+          password: this.password,
+        };
+        const { data } = await loginUser(userData);
+        console.log(data.user.username);
+        this.logMessage = `${data.user.username}님 환영합니다.`;
+      } catch (error) {
+        console.log(error.response.data);
+        this.logMessage = error.response.data;
+      } finally {
+        this.initForm();
+      }
+    },
+    initForm() {
+      (this.username = ''), (this.password = '');
+    },
+  },
+};
+</script>
+
+<style></style>
