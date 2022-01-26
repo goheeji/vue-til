7db50aec4ab37fbeb0f2ccaa6c930ff4f5023aa8
goheeji <gmlwl0489@daum.net>
2022-01-26 14:34:57 +0900

    로그아웃 구현

--- src/components/common/AppHeader.vue
+++ src/components/common/AppHeader.vue
@@ -6,17 +6,41 @@
       </router-link>
     </div>
     <div class="navigations">
-      <router-link to="/login">로그인</router-link>
-      <router-link to="/signup">회원가입</router-link>
+      <!-- 1 -->
+      <!-- <template v-if="$store.getters.isLogin"> -->
+      <template v-if="isUserLogin">
+        <span class="username">{{ $store.state.username }}</span>
+        <a href="javascript:;" @click="logoutUser">로그아웃</a>
+      </template>
+      <!-- 2 -->
+      <template v-else>
+        <router-link to="/login">로그인</router-link>
+        <router-link to="/signup">회원가입</router-link>
+      </template>
     </div>
   </header>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    isUserLogin() {
+      return this.$store.getters.isLogin;
+    },
+  },
+  methods: {
+    logoutUser() {
+      this.$store.commit('clearUsername');
+      this.$router.push('/login');
+    },
+  },
+};
 </script>
 
 <style scoped>
+.username {
+  color: white;
+}
 header {
   display: flex;
   justify-content: space-between;
