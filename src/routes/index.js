e091d06e58fa05832defd59efa7a3ee80cffeb23
goheeji <gmlwl0489@daum.net>
2022-01-28 13:29:26 +0900

    router-navigation-guard

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '@/store/index';
 // import LoginPage from '@/views/LoginPage.vue';
 // import SignupPage from '@/views/SignupPage.vue';
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
   mode: 'history',
   routes: [
     {
@@ -23,14 +24,19 @@ export default new VueRouter({
     {
       path: '/main',
       component: () => import('@/views/MainPage.vue'),
+      // 라우터의 부가정보를 정의할 수 있는 속성
+      // auth는 임의로 정의한 속성 이름.
+      meta: { auth: true },
     },
     {
       path: '/add',
       component: () => import('@/views/PostAddPage.vue'),
+      meta: { auth: true },
     },
     {
       path: '/post/:id',
       component: () => import('@/views/PostEditPage.vue'),
+      meta: { auth: true },
     },
     {
       path: '*',
@@ -38,3 +44,19 @@ export default new VueRouter({
     },
   ],
 });
+/**
+ * 콜백의 인자
+ * to: 이동하려는 페이지
+ * from: 현재 페이지
+ * next: 페이지 이동할 때 호출하는 API
+ */
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.auth && !store.getters.isLogin) {
+    console.log('인증이 필요합니다.');
+    next('/login');
+    return; // next('/login'); 한 후 또 다시 next();가 호출되므로 return 문을 사용한다.
+  }
+  next(); //next를 호출해야만 다음 페이지로 이동한다.
+});
+export default router;
